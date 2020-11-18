671136a6ce5241b9f197ab2b7f53aa34bd19d118
Monica82mg <monilugcasti@gmail.com>
2020-11-18 15:32:23 +0100

    Ejercicio 2 de la unidad 2 resuelto

---
+++ Ejercicios 1ª Evaluación/src/unidad2/Conversor.java
@@ -0,0 +1,23 @@
+package unidad2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Conversor {
+
+	public static void main(String[] args) throws IOException {
+		float euros;
+		float dolares;
+		String linea;
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("introduce un numero:");
+		linea = in.readLine();
+		euros = Float.parseFloat(linea) ;
+		dolares = euros * 1.17f;
+		System.out.printf("%-15.2f", dolares);
+		
+	}
+
+}
